8e5e849510de8b0ffd804ae0f58dfa1085e1c8f2
Sanya Gupta <guptasanya366@gmail.com>
2026-03-17 15:44:12 +0530

    converted to binary output label

--- src/data_processing/label_conversion.py
+++ src/data_processing/label_conversion.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-# Read from partner's output
-df = pd.read_csv('C:\Users\projects\Fairness-Aware-Loan-Prediction-\data\raw\External_Cibil_Dataset.csv')  # adjust filepath
+df = pd.read_csv(r'C:\Users\projects\Fairness-Aware-Loan-Prediction-\data\processed\feature_reduction\External_Cibil_Dataset.csv')
 
-# Convert target
 priority_mapping = {'P1': 0, 'P2': 0, 'P3': 1, 'P4': 1}
-df['loan_status_binary'] = df['priority_column'].map(priority_mapping)
-df.drop(columns=['priority_column'], inplace=True)
 
-# Save to processed
-df.to_csv('data/processed/loan_data_processed.csv', index=False)
-print("Saved to data/processed/loan_data_processed.csv")
-print(df['loan_status_binary'].value_counts(normalize=True) * 100)
+# Check distribution BEFORE dropping
+print(df['Approved_Flag'].value_counts(normalize=True) * 100)
+
+# Map target
+df['loan_status_binary'] = df['Approved_Flag'].map(priority_mapping)
+
+# Drop original
+df.drop(columns=['Approved_Flag'], inplace=True)
+
+# Save properly
+df.to_csv(r'C:\Users\projects\Fairness-Aware-Loan-Prediction-\data\processed\binaryLabel\label_conversion.csv', index=False)
+
+print("Saved to data/processed/label_conversion.csv")
